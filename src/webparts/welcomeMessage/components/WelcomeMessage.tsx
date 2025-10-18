3eebbb892edca7225509a033a08f8938f7f32e30
steventhestudent <sgonza272@calstatela.edu>
2025-10-17 18:20:34 -0700

    WelcomeMessage (Welcome <user>, search don't work)

--- src/webparts/welcomeMessage/components/WelcomeMessage.tsx
+++ src/webparts/welcomeMessage/components/WelcomeMessage.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import styles from './WelcomeMessage.module.scss';
 import type { IWelcomeMessageProps } from './IWelcomeMessageProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import {WelcomeSearch} from "./WelcomeSearch";
 
 export default class WelcomeMessage extends React.Component<IWelcomeMessageProps> {
   public render(): React.ReactElement<IWelcomeMessageProps> {
     const {
-      description,
-      isDarkTheme,
-      environmentMessage,
+      // description,
+      // isDarkTheme,
+      // environmentMessage,
       hasTeamsContext,
       userDisplayName
     } = this.props;
@@ -16,27 +17,9 @@ export default class WelcomeMessage extends React.Component<IWelcomeMessageProps
     return (
       <section className={`${styles.welcomeMessage} ${hasTeamsContext ? styles.teams : ''}`}>
         <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
+          <h2>Welcome to the Public Defender Resource Center, {escape(userDisplayName)}!</h2>
         </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
+        <WelcomeSearch />
       </section>
     );
   }
---
+++ src/webparts/welcomeMessage/components/WelcomeSearch.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {Label} from "@fluentui/react";
+
+export const WelcomeSearch = () => (<form role="search">
+	<Label>
+		Search:
+		<input role="combobox" type="search" aria-autocomplete="list" aria-controls="ms-searchux-popup-0" accessKey="S"
+			   spellCheck="false" autoComplete="off" autoCorrect="false" data-nav="true" data-tab="true" maxLength={500}
+			   placeholder="Search..." className="" aria-expanded="true" name="WelcomeSearch" />
+		<button>↠</button>
+	</Label>
+</form>);
