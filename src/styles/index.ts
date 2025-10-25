8c188ad91abd52545d23ff9e445566457172c7e9
steventhestudent <sgonza272@calstatela.edu>
2025-10-24 21:32:59 -0700

    add ThemeInjector (ApplicationCustomizer) (5)

---
+++ src/styles/index.ts
@@ -0,0 +1,3 @@
+// import '@styles'; // imports src/styles/index.ts                  <--- for some reason this alias doesn't work, even though it points to a typescript file...
+import "./SharePointStyleOverride.css"
+import "@dist/tailwind.css"; // to do: comment out in production? (do webparts all include a copy of it!?)
