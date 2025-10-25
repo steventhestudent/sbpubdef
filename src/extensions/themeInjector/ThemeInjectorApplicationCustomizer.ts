6d34b16073e8da2ba4b78cc7a4a53125a5357290
steventhestudent <sgonza272@calstatela.edu>
2025-10-24 19:31:16 -0700

    add ThemeInjector (ApplicationCustomizer) (4)

--- src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
+++ src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
@@ -1,24 +1,32 @@
 import { Log } from '@microsoft/sp-core-library';
 import { BaseApplicationCustomizer } from '@microsoft/sp-application-base';
 import * as strings from 'ThemeInjectorApplicationCustomizerStrings';
-import "@dist/tailwind.css"; // comment out in production?
+import "../../styles/SharePointStyleOverride.css"
+import "@dist/tailwind.css"; // to do: comment out in production? (do webparts all include a copy of it!?)
 
 export default class ThemeInjectorApplicationCustomizer extends BaseApplicationCustomizer<{}> {
   public async onInit(): Promise<void> {
     Log.info('ThemeInjectorApplicationCustomizer', `Initialized ${strings.Title}`);
     console.log(`themeInjector.properties:`, this.properties);
 
-    // inject <style> (ensure these tailwind classes exist)
-    const text = document.createTextNode(`
-      .\\!max-w-full {max-width: 100% !important;}
-    `);
-    const styles = document.createElement('style');
-    styles.appendChild(text);
-    document.body.appendChild(styles);
+    // const SharePointStyleOverride = document.createElement('style');
+    // SharePointStyleOverride.appendChild(document.createTextNode(`
+    //   /* override sharepoint max-width */
+    //   .CanvasComponent, .CanvasZoneSectionContainer {max-width: 100% !important;}
+    // `));
+    // document.body.appendChild(SharePointStyleOverride);
 
-    document.querySelector(".CanvasComponent")!.children[0].classList.add("!max-w-full");
-    Array.from(document.querySelectorAll(".CanvasZoneSectionContainer")).forEach((el) => el.classList.add("!max-w-full"));
-    // document.querySelector("div[aria-label=\"DismissibleAnnouncementStrip\"]")?.querySelector("");
+    // // inject <style> (ensure these tailwind classes exist)
+    // const text = document.createTextNode(`
+    //   .\\!max-w-full {max-width: 100% !important;}
+    // `);
+    // const styles = document.createElement('style');
+    // styles.appendChild(text);
+    // document.body.appendChild(styles);
+    //
+    // document.querySelector(".CanvasComponent")!.children[0].classList.add("!max-w-full");
+    // Array.from(document.querySelectorAll(".CanvasZoneSectionContainer")).forEach((el) => el.classList.add("!max-w-full"));
+    // // document.querySelector("div[aria-label=\"DismissibleAnnouncementStrip\"]")?.querySelector("");
 
     return Promise.resolve();
   }
