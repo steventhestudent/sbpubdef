d887c31783a69244b42e1f23a667fc3cfce32dcf
steventhestudent <sgonza272@calstatela.edu>
2025-10-25 22:25:01 -0700

    add ThemeInjector (ApplicationCustomizer) (10) — ThemeInjector.CompactModeBtn (i.e. rename MarginBtn) + margin/padding animation

--- src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
+++ src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
@@ -20,9 +20,9 @@ export default class ThemeInjectorApplicationCustomizer extends BaseApplicationC
     // Array.from(document.querySelectorAll(".CanvasZoneSectionContainer")).forEach((el) => el.classList.add("!max-w-full"));
     // // document.querySelector("div[aria-label=\"DismissibleAnnouncementStrip\"]")?.querySelector("");
 
-    function addMarginLessStylesheet(): void {
+    function addCompactModeStylesheet(): void {
       const stylesheet = document.createElement('style');
-      stylesheet.id = "themeInjector.MarginLessStylesheet";
+      stylesheet.id = "themeInjector.CompactModeStylesheet";
       stylesheet.appendChild(document.createTextNode(`
           .CanvasZone {padding: 0 !important;}
           .CanvasZoneSectionContainer .CanvasSection {padding: 0 !important;}
@@ -30,31 +30,39 @@ export default class ThemeInjectorApplicationCustomizer extends BaseApplicationC
         `));
       document.head.appendChild(stylesheet);
     }
-    function toggleMarginLessStylesheet(e: MouseEvent): void {
-      if (document.querySelector("#themeInjector\\.MarginLessStylesheet")) {
+    function toggleCompactModeStylesheet(e: MouseEvent): void {
+      document.querySelectorAll(".CanvasZone, .CanvasZoneSectionContainer .CanvasSection, .CanvasSection .ControlZone").forEach((el: HTMLElement) => {
+        el.style.transition = 'margin 200ms ease-in-out, padding 200ms ease-in-out';
+      });
+      setTimeout(() => updateTooltip(e.target as HTMLButtonElement));
+      if (document.querySelector("#themeInjector\\.CompactModeStylesheet")) {
         (e.target as HTMLButtonElement).innerText = "🔳";
         localStorage.setItem("ThemeInjector.MarginModeFlag", "true");
-        return document.querySelector("#themeInjector\\.MarginLessStylesheet")?.remove();
+        return document.querySelector("#themeInjector\\.CompactModeStylesheet")?.remove();
       }
       localStorage.removeItem("ThemeInjector.MarginModeFlag");
       (e.target as HTMLButtonElement).innerText = "◼️️";
-      addMarginLessStylesheet();
+      addCompactModeStylesheet();
     }
-    function MarginBtn() : HTMLButtonElement {
+    function updateTooltip(btn: HTMLButtonElement): void {
+      btn.title = (document.querySelector("#themeInjector\\.CompactModeStylesheet") ? "Compact Mode" : "SharePoint Mode") +
+                  " —  Toggle Webpart Margin / Padding (ThemeInjector.CompactModeStylesheet)";
+    }
+    function CompactModeBtn() : HTMLButtonElement {
       const btn = document.createElement("button");
-      btn.innerText = document.querySelector("#themeInjector\\.MarginLessStylesheet") ? "◼️" : "🔳";
+      btn.innerText = document.querySelector("#themeInjector\\.CompactModeStylesheet") ? "◼️" : "🔳";
       btn.style.fontSize = "18px";
       btn.style.cursor = "pointer";
       btn.style.textShadow = "0 0 4px #03787c";
       btn.style.opacity = "0.75";
-      btn.title = "Toggle Webpart Margin / Padding (ThemeInjector.MarginLessStylesheet)";
-      btn.addEventListener("click", toggleMarginLessStylesheet);
+      updateTooltip(btn);
+      btn.addEventListener("click", toggleCompactModeStylesheet);
       return btn;
     }
     const getToolbar: () => HTMLElement | null = () => document.querySelector(".fui-Toolbar .ms-OverflowSet:last-of-type, .fui-FluentProvider .ms-OverflowSet:last-of-type"); // add after ↙↗ 'Expand Content' btn. fui-FluentProvider = Site Contents view
-    const toolbarInsertion: () => void = () => getToolbar()?.appendChild(MarginBtn());
+    const toolbarInsertion: () => void = () => getToolbar()?.appendChild(CompactModeBtn());
     (function pollInsert() {if (!getToolbar()) setTimeout(pollInsert, 333); else setTimeout(toolbarInsertion, 666);})();
-    if (!localStorage.getItem("ThemeInjector.MarginModeFlag")) addMarginLessStylesheet();
+    if (!localStorage.getItem("ThemeInjector.MarginModeFlag")) addCompactModeStylesheet();
 
     return Promise.resolve();
   }
