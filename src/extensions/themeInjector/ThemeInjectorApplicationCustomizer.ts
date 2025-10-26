7f57b18d53a027909337c13a41047a84cde683bc
steventhestudent <sgonza272@calstatela.edu>
2025-10-26 04:39:48 -0700

    add ThemeInjector (ApplicationCustomizer) (12) — DismissibleAnnouncemnetStrip (2)

--- src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
+++ src/extensions/themeInjector/ThemeInjectorApplicationCustomizer.ts
@@ -13,28 +13,25 @@ export default class ThemeInjectorApplicationCustomizer extends BaseApplicationC
     */
     function DismissibleAnnouncementStrip(): HTMLDivElement {
       const strip = document.createElement('div');
+      strip.appendChild(document.createTextNode("Covid-19 message / wash your hands! The wifi password is: (Click me)"));
       strip.id = 'DismissibleAnnouncementStrip';
       strip.style.textAlign = "center";
-      strip.style.backgroundColor = "black";
-      strip.style.color = "white";
+      strip.style.backgroundColor = "rgb(183, 183, 183)";
+      strip.style.color = "black";
       strip.style.padding = "4px";
       strip.style.cursor = "pointer";
       if (localStorage.getItem("ThemeInjector.DismissibleAnnouncementStripCollapsed")) strip.style.height = "5px";
       strip.addEventListener('click', (event: MouseEvent) => {
-        // strip.style.position = "absolute";
-        // strip.style.left = "50%";
-        // strip.style.marginLeft = "-25px";
-        // strip.style.marginTop = "-25px";
-        // strip.style.width = "50px";
         if (strip.style.height) localStorage.removeItem("ThemeInjector.DismissibleAnnouncementStripCollapsed"); else localStorage.setItem("ThemeInjector.DismissibleAnnouncementStripCollapsed", "true");
+        strip.style.fontSize = strip.style.height ? "" : "0px";
+        strip.className = strip.style.height ? "" : "collapsed";
         strip.style.height = strip.style.height ? "" : "5px";
       });
-      strip.appendChild(document.createTextNode("Covid-19 message / wash your hands! The wifi password is: (Click me)"));
       return strip;
     }
     const getCanvasComponent: () => HTMLElement | null = () => document.querySelector('.CanvasComponent');
-    const stripInsertion: () => void = () => getCanvasComponent()?.insertBefore(DismissibleAnnouncementStrip(), getCanvasComponent()!.children[0]);
-    (function pollInsert() {if (!getCanvasComponent()) setTimeout(pollInsert, 333); else setTimeout(stripInsertion, 0);})();
+    const stripInsertion: () => void = () => getCanvasComponent()?.parentNode!.insertBefore(DismissibleAnnouncementStrip(), getCanvasComponent());
+    (function pollInsert() {if (!getCanvasComponent()) setTimeout(pollInsert, 333); else setTimeout(stripInsertion, 666);})();
 
 
     /*
@@ -47,6 +44,8 @@ export default class ThemeInjectorApplicationCustomizer extends BaseApplicationC
           .CanvasZone {padding: 0 !important;}
           .CanvasZoneSectionContainer .CanvasSection {padding: 0 !important;}
           .CanvasSection .ControlZone {margin: 0 !important; padding: 0 !important;}
+          
+          #DismissibleAnnouncementStrip.collapsed {margin-top: 4px; margin-bottom: 4px;}
         `));
       document.head.appendChild(stylesheet);
     }
