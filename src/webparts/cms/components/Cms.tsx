aec4fcee3489cfc082e5faae69e2688cd6e709bf
steventhestudent <sgonza272@calstatela.edu>
2025-10-28 01:00:55 -0700

    add cms webpart (/SitePages/cms.aspx)

---
+++ src/webparts/cms/components/Cms.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import type { ICmsProps } from "./ICmsProps";
+
+export default class Cms extends React.Component<ICmsProps> {
+	public render(): React.ReactElement<ICmsProps> {
+		return <div>cms</div>;
+	}
+}
---
+++ src/webparts/cms/components/ICmsProps.ts
@@ -0,0 +1,7 @@
+export interface ICmsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
