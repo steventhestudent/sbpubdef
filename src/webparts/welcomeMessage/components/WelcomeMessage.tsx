78c3bef4c301f6c43a06bd7d44941149fb23d8cb
steventhestudent <sgonza272@calstatela.edu>
2026-02-02 23:52:22 -0800

    WelcomeMessage username popup templating

--- src/webparts/welcomeMessage/components/WelcomeMessage.tsx
+++ src/webparts/welcomeMessage/components/WelcomeMessage.tsx
@@ -23,19 +23,46 @@ function PDIntranetViewWrapper(
 				<div className="rounded-xl border border-[var(--webpart-border-color)] p-6 shadow-sm bg-[#f9f9f9]">
 					<h2 className="text-center text-xl font-semibold text-slate-800">
 						Welcome to the Public Defender Resource Center,&nbsp;
-						<span className="relative group cursor-pointer underline">
-							{escape(userDisplayName)}!
-							<div className="absolute right-5 top-[2.3em] left-0 bg-slate-300 text-xs overflow-auto max-h-[8em] ml-[1em] text-left hidden group-hover:block">
-								<ul>
+						<span className="relative group">
+							<span className="underline cursor-pointer text-nowrap">
+								{escape(userDisplayName)}
+							</span>
+							<span className="absolute bottom-[-0.44em] right-[-0.4em] text-gray-500 text-sm">
+								⌄
+							</span>
+							<div
+								className="w-[14em] border-1 border-[#b6b6b6] rounded-b box-border absolute left-[calc(50%-7.666em)] top-[2em] bg-slate-200 text-xs overflow-hidden max-h-[50px] ml-[1em] text-left opacity-0
+							invisible transition-opacity duration-333 ease-in-out
+							group-hover:visible group-hover:opacity-100"
+							>
+								<div
+									className="cursor-pointer absolute float-left w-[48px] h-[48px] bg-slate-300 border-1 border-l-0 border-t-0 border-[#b6b6b6] text-center relative"
+									title="User Avatar"
+								>
+									<div className="opacity-50 absolute top-[50%] left-[50%] ml-[-0.6333em] mt-[-0.6333em]">
+										❌
+									</div>
+								</div>
+								<ol
+									title="Entra ID Security Groups (Group Memberships):"
+									className="p-0.5 absolute left-[48px] w-[calc(100%-48px)] overflow-auto max-h-full scrollbar-thin"
+								>
+									Groups ({userGroupNames.length}):
 									{userGroupNames.map((userGroupName) => (
 										<li
-											className="list-disc ml-5"
+											className="list-decimal ml-3"
 											key={userGroupName}
 										>
 											{userGroupName}
 										</li>
 									))}
-								</ul>
+								</ol>
+								<div
+									className="absolute bottom-0 right-0 cursor-pointer text-xl opacity-75 hover:opacity-100 text-right"
+									title="User Settings"
+								>
+									⚙️
+								</div>
 							</div>
 						</span>
 					</h2>
