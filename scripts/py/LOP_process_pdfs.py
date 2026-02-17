d5bd1d2c44ad42b5af778895e752b15de5a19b8e
steventhestudent <sgonza272@calstatela.edu>
2026-02-16 21:31:31 -0800

    procedurechecklist prepare to process lists and blocks (3)

--- scripts/py/LOP_process_pdfs.py
+++ scripts/py/LOP_process_pdfs.py
@@ -12,16 +12,16 @@ if not os.path.exists(LOCAL_OUT_DIR + "/img"): os.makedirs(LOCAL_OUT_DIR + "/img
 # to modify document library OUT_DIR, see: sbpubdef.local_upload
 
 procedure_checklists = []
-
+ignore_categories = ["Forms", "Contacts", "827 Petitions to Access Juvenile Case Files", "Records Requests"]
 # walk resource dir
 ignored = []
 for root, dirs, files in os.walk(SCRAPED_LOP_PAGE_RESOURCES):
     for filename in files:
         filepath = os.path.join(root, filename)
         category = os.path.basename(root) # dir name == header/category the pdf is under (Interpreting Resources, Forms, etc.)
-        if category == "Forms" or category == "Contacts": continue # not a procedure
+        if category in ignore_categories: continue # not a procedure
         if not(filename.endswith('.pdf')):
             if not(filename.endswith('.DS_Store')): ignored.append(f'{category}/{filename}')
             continue
         procedure_checklists.append(ProcedureChecklist(filepath, category, LOCAL_OUT_DIR))
-    if len(procedure_checklists) == 1: break # debug 1 pdf
+    # if len(procedure_checklists) == 1: break # debug 1 pdf
--- scripts/py/sbpubdef/lop/procedure_checklist/upload.py
+++ scripts/py/sbpubdef/lop/procedure_checklist/upload.py
@@ -1,17 +1,29 @@
+import os.path
+from enum import Enum
+
 from . import ProcedureChecklist
 from ...local_upload import authenticate, get_site_id, get_drive_id, get_list_id, upload_file as _upload_file, add_list_item as _add_list_item
 
-# authenticate()
-# site_id = get_site_id("PD-Intranet")
-# drive_id = get_drive_id(site_id, "user_uploads")
-# list_id = get_list_id(site_id, "LOPProcedureChecklist")
+class UPDATE_MODES(Enum):
+    NO_API_REQUESTS = 0
+    JSON_ONLY = 1
+    ALL = 2
+
+# config
+UPDATE_MODE = UPDATE_MODES.NO_API_REQUESTS
+
+if not(UPDATE_MODE == UPDATE_MODES.NO_API_REQUESTS):
+    authenticate()
+    site_id = get_site_id("PD-Intranet")
+    drive_id = get_drive_id(site_id, "user_uploads")
+    list_id = get_list_id(site_id, "LOPProcedureChecklist")
 
 def upload_file(file_path):
-    return file_path
+    if UPDATE_MODE == UPDATE_MODES.NO_API_REQUESTS or (UPDATE_MODE == UPDATE_MODES.JSON_ONLY and not(file_path.endswith('.json'))): return "https://csproject25.sharepoint.com/sites/PD-Intranet/user_uploads/resource/LOP/ProcedureChecklist/" + os.path.basename(file_path)
     return _upload_file(site_id, drive_id, file_path, "resource/LOP/ProcedureChecklist")
 
 def add_list_item(procedure: ProcedureChecklist): # must use internal column names found w/ get_list_columns()
-    return "proc-" + procedure.filename
+    if UPDATE_MODE.value < UPDATE_MODES.ALL.value: return "https://csproject25.sharepoint.com/sites/PD-Intranet/lists/LOPProcedureChecklist/0_.000" # we don't use this so it's ok to always use 0_.000
     return _add_list_item(site_id, list_id, { # sharepoint list columns
         "Category": procedure.category,
         "Title": procedure.title[0] if isinstance(procedure.title, list) else procedure.title, # just in case our title handling is buggy
